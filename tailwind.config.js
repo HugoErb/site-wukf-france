ab2f305059ad87d9099e0da9aa87e6ebafad51ab
HugoErb <erb.hugo@yahoo.fr>
2026-08-02 14:12:33 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,3 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = { content: ["./**/*.html", "./assets/js/**/*.js"], theme: { extend: { colors: { reddeep: "#8F1D21", charcoal: "#151515", ivory: "#F8F3EA", stone: "#D7D0C4", goldsoft: "#B89A5E", ink: "#24211D" }, fontFamily: { sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"], display: ["Arial", "ui-sans-serif", "system-ui", "sans-serif"] }, boxShadow: { line: "0 1px 0 rgba(21,21,21,.12)" } } }, plugins: []
+};
